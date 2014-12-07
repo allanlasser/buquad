c1bac32c850097125a529efff1472ab9e3e2c450
Allan Lasser <lasser.allan@gmail.com>
2014-12-07 17:24:12 -0500

    fixes front-page

--- front-page.php
+++ front-page.php
@@ -15,18 +15,18 @@ if ($features_query->have_posts()) :
     while($features_query->have_posts()) :
         $features_query->the_post();
 
-        $post_author = post_author;
+        $post_author = post_author();
         $post_author_name = get_the_author_meta('display_name', $post_author);
         $post_author_url = get_author_posts_url($post_author);
         $post_image = get_the_post_thumbnail(get_the_id(), 'large');
 ?>
     <div class="featured-article container">
         <div class="featured-article-image">
-            <a href="<?php the_permalink; ?>"><?php echo $post_image; ?></a>
+            <a href="<?php the_permalink(); ?>"><?php echo $post_image; ?></a>
         </div>
         <div class="featured-article-description">
             <dfn>Feature Story</dfn>
-            <h2><a href="<?php the_permalink; ?>"><?php the_title(); ?></a></h2>
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
             <h3>by <a href="<?php echo $post_author_url; ?>"><?php echo $post_author_name; ?></a></h3>
             <p><?php the_excerpt(); ?></p>
         </div>
